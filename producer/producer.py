a3222e8ceb44b53858ad308e74209ee9ccaede77
YOSSEF <hazemyossef0@gmail.com>
2024-11-26 14:08:25 +0200

    update producer

--- producer/producer.py
+++ producer/producer.py
@@ -8,25 +8,32 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-# function to Read the CSV file and produce dictionary row data 
 def read_and_produce(csv_file, topic):
     data = pd.read_csv(csv_file)
 
     print(f"Loaded {len(data)} rows from {csv_file}")
     print(data.head())
 
-    # Send each row to Kafka
+    transaction_id = 1  # make transaction_id incrementally
     for _, row in data.iterrows():
-        row_dict = row.to_dict()  
-        producer.send(topic, value=row_dict)
-        print(f"Produced: {row_dict}")
-    
-    # Ensure all messages are sent
-    producer.flush()
+        row_dict = row.to_dict()
+        true_score = row_dict.pop("Credit Score")  # Remove true_score from features dictionary
+        message = {
+            "transaction_id": transaction_id,
+            "features": row_dict,
+            "true_score": true_score
+        }
+
+        producer.send(topic, value=message)
+        print(f"Produced: Transaction ID {transaction_id}")
+
+        transaction_id += 1  
+
+    producer.flush() # Waiting untile all the messages sent
     print("Finished producing messages.")
 
 
 if __name__ == "__main__":
-    topic_name = "credit_scoring" # I creted a topic with this name in my Kafka cluster containg 3 partitions
-    csv_file = "C:\Users\Yossef Hazem\real-time-payment-scoring\Data\testing_features_sc.csv"  
+    topic_name = "credit-scoring" # I created a topic with this name in my Kafka cluster containg 3 partitions
+    csv_file = r"Data\final_data.csv"  
     read_and_produce(csv_file, topic_name)
